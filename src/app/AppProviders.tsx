e6e639a2e401f640c960bcc75d3774af538a56d1
mohamed aymen <155321378+aymenbech@users.noreply.github.com>
2026-09-20 12:31:16 +0200

    feat(i18n): register language provider

--- src/app/AppProviders.tsx
+++ src/app/AppProviders.tsx
@@ -3,11 +3,14 @@
 import React, {PropsWithChildren} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {AuthProvider} from './AuthContext';
+import {LanguageProvider} from './LanguageContext';
 
 export function AppProviders({children}: PropsWithChildren) {
   return (
     <SafeAreaProvider>
-      <AuthProvider>{children}</AuthProvider>
+      <LanguageProvider>
+        <AuthProvider>{children}</AuthProvider>
+      </LanguageProvider>
     </SafeAreaProvider>
   );
 }
